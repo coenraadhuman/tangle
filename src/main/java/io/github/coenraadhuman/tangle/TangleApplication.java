49821a7d56e87052b5f43678928a54c6b38d82c0
Coenraad Human <coenraad.human@proton.me>
2025-02-10 23:28:19 +0100

    feat: initial project setup

---
+++ src/main/java/io/github/coenraadhuman/tangle/TangleApplication.java
@@ -0,0 +1,12 @@
+package io.github.coenraadhuman.tangle;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TangleApplication {
+
+	public static void main(String[] args) {
+		log.info("It is alive!");
+	}
+
+}
---
+++ src/test/java/io/github/coenraadhuman/tangle/TangleApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.coenraadhuman.tangle;
+
+import org.junit.jupiter.api.Test;
+
+class TangleApplicationTests {
+
+	@Test
+	void executeMain() {
+		TangleApplication.main(new String[] {});
+	}
+
+}
